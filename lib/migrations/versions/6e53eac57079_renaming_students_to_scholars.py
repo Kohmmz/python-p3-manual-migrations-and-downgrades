ad05fc9fc23c244946ad5f73526d5e534dfcca85
Kohmmz <jeremiahkoome1@gmail.com>
2025-03-06 15:07:41 +0300

    rename students table to scholars and add migration scripts

---
+++ lib/migrations/versions/6e53eac57079_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 6e53eac57079
+Revises: 6e4d5b4eb652
+Create Date: 2025-03-06 14:59:25.585099
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6e53eac57079'
+down_revision = '6e4d5b4eb652'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
